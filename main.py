a731bf63e3eb5872f9ac0cbeb62a1f7ede5320ce
JuanCruzV4 <83989306+JuanCruzV4@users.noreply.github.com>
2026-03-30 11:35:38 -0300

    Integrate weather API and update email notifications
    Added weather API integration to check for rain and send motivational emails accordingly.

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ import os
 
 my_mail = os.environ.get("MY_MAIL")
 password = os.environ.get("MY_PASSWORD")
+my_lat = os.environ.get("MY_LAT")
+my_lon = os.environ.get("MY_LON")
+my_appid = os.environ.get("MY_APPID")
+
+parameters = {
+    "lat": my_lat,
+    "lon": my_lon,
+    "appid": my_appid,
+    "cnt": 4
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+
+rain = False
+
+for item in weather_data["list"]:
+    condition_code = item["weather"][0]["id"]
+    if condition_code < 700:
+        rain = True
 
 now = dt.datetime.now()
 day_of_week = now.weekday()
@@ -17,13 +39,38 @@ if day_of_week == 0:
         quote = random.choice(file)
         for clave, valor in mails_dict.items():
             try:
+                if rain:
+                    with smtplib.SMTP("smtp.gmail.com") as connection:
+                        connection.starttls()
+                        connection.login(user=my_mail, password=password)
+                        connection.sendmail(
+                            from_addr=my_mail,
+                            to_addrs=clave[1],
+                            msg=f"Subject:Monday Motivation to {clave[0]}\n\n{quote}\n\n Hoy llueve! Llevate ☂️".encode("utf-8")
+                        )
+                else:
+                     with smtplib.SMTP("smtp.gmail.com") as connection:
+                        connection.starttls()
+                        connection.login(user=my_mail, password=password)
+                        connection.sendmail(
+                            from_addr=my_mail,
+                            to_addrs=clave[1],
+                            msg=f"Subject:Monday Motivation to {clave[0]}\n\n{quote}".encode("utf-8")
+                        )
+            except Exception as e:
+                print("Error sendind email",e)
+else:
+    if rain:
+        for clave, valor in mails_dict.items():
+            try:
                 with smtplib.SMTP("smtp.gmail.com") as connection:
                     connection.starttls()
                     connection.login(user=my_mail, password=password)
                     connection.sendmail(
                         from_addr=my_mail,
                         to_addrs=clave[1],
-                        msg=f"Subject:Monday Motivation to {clave[0]}\n\n{quote}".encode("utf-8")
-                    )
+                        msg=f"Subject:Ojota que hoy llueve\n\n Hoy llueve! Llevate ☂️".encode("utf-8")
+                        )
             except Exception as e:
                 print("Error sendind email",e)
+    
